b024daf1f14e19b1c66f7a7de0302a0338ffb790
mansourlahham14 <mansourlahham14@gmail.com>
2026-02-15 23:30:09 +0100

    configurazione API meteo

---
+++ JS/config.js
@@ -0,0 +1,13 @@
+// Configurazione API OpenWeather
+
+const CONFIG = {
+  // Chiave API ottenuta da https://openweathermap.org/api
+  API_KEY: '0619cc042e8b033f1d1a5565bd7f68e7',
+  
+  BASE_URL: 'https://api.openweathermap.org/data/2.5',
+  
+  UNITS: 'metric',
+  LANG: 'it'
+};
+
+export default CONFIG;
